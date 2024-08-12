5d20deea7bc3dbfd9f2a664ca042c1b59ae50370
pabloacisera <pabloacisera2021@gmail.com>
2024-08-12 13:25:35 -0300

    actualizacion backend

--- src/index.js
+++ src/index.js
@@ -22,17 +22,6 @@ app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Obtener el directorio actual usando import.meta.url
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Servir archivos estáticos desde la carpeta frontend
-app.use(express.static(path.join(__dirname, '..', 'frontend')));
-
-// Ruta para servir index.html en la raíz
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
-});
 
 // Rutas del servidor
 app.use('/pay', routerPayments);
